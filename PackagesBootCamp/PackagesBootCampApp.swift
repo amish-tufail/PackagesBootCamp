2469511b83966efc9201756da44f2189779e5b44
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2024-03-24 16:29:16 +0500

    Initial Commit

---
+++ PackagesBootCamp/PackagesBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackagesBootCampApp.swift
+//  PackagesBootCamp
+//
+//  Created by Amish Tufail on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PackagesBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
